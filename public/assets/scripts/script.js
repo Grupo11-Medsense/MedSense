a0924a65a29f44591889e374dbe324869a0cc9de
Thiago Prates <thiago.prates@sptech.school>
2025-06-05 03:37:56 -0300

    Subindo login do suporte

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -198,7 +198,50 @@ function entrar() {
 
     if (emailVar == "" || senhaVar == "") {
         alert('Mensagem de erro para todos os campos em branco');
+
+        return
     }
+
+    if (emailVar.includes("@medsense")) {
+        fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            emailServer: emailVar,
+            senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+            console.log(resposta);
+
+            resposta.json().then(json => {
+                console.log(json);
+                console.log(JSON.stringify(json));
+                sessionStorage.EMAIL_USUARIO = json.email;
+                sessionStorage.NOME_USUARIO = json.nome;
+                sessionStorage.ID_USUARIO = json.idUsuario;
+                alert(`Seja bem-vindo(a)`);
+                window.location = "BobIA.html";
+
+            });
+        } else {
+
+            console.log("Houve um erro ao tentar realizar o login!");
+
+            resposta.text().then(texto => {
+                console.error(texto);
+            });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+    }
+    else {
 
     console.log("FORM LOGIN: ", emailVar);
     console.log("FORM SENHA: ", senhaVar);
@@ -242,7 +285,7 @@ function entrar() {
     })
 
 }
-
+}
 
 
 
